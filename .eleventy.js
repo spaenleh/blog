2f935c116d1791ca162be44dccdc13cc79489200
spaenleh <spaenleh@gmail.com>
2024-04-19 07:51:38 +0200

    fix: update path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+  // add html base plugin
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   // copy base bundle.css file
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("space-grotesk.ttf");
@@ -12,4 +17,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("displayDate", function (date, locale) {
     return new Intl.DateTimeFormat(locale, { dateStyle: "long" }).format(date);
   });
+  return {
+    pathPrefix: "/blog/",
+  };
 };
